aa0ad9ade6a3648e3cbbaa97a752b11609f8c8a2
Jonas <jonas.alves@live.com>
2019-04-29 23:00:26 -0300

    Added material UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Typography from '@material-ui/core/Typography'
 import './App.css';
+export default class App extends Component {
+  state = {
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Typography variant='display1' align='center' className='typo' gutterBottom>
+          Exercises
+        </Typography>
+      </div>
+    )
+  }
 }
-
-export default App;
