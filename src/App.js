d7d7100db2a5def4ae632f9c79130ab8487bd6ec
Jonas <jonas.alves@live.com>
2019-05-03 07:37:52 -0300

    Theming the Profile components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import Profile from './components/Profile/Page'
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#0a0a0a',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      main: '#181818',
+    },
+    type: "dark"
+  }
+});
 
 const App = () => {
   return (
     <>
       <CssBaseline />
-      <div className="App">
-        <Profile>
-        </Profile>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Profile>
+          </Profile>
+        </div>
+      </MuiThemeProvider>
     </>
   );
 }
